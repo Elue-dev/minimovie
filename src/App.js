a722f693095d84daade63017a07c07bc5ee7012d
Elue-dev <eluewisdom@gmail.com>
2021-11-24 07:26:34 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { MovieList } from './MovieList';
 import { MovieDetails } from './MovieDetails';
-import { Home } from './Home';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Routes>
-        <Route  exact path='/' element={<Home />} />
-        <Route exact path='/movie' element={<MovieList/>} />
+        <Route exact path='/' element={<MovieList/>} />
           <Route path='/movie/:movieId' element={<MovieDetails/>} />
       </Routes>
     </div>
